d9d9f63b60699aee7281e46ffccad8cd46479adf
Paula Brouwer <annepaulinebrouwer@hotmail.com>
2016-03-02 18:31:32 +0100

    Add controllers and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  get 'review/index'
+  # get 'review/index'
 
-  get 'review/new'
+  # get 'review/new'
 
-  get 'review/create'
+  # get 'review/create'
 
-  get 'restaurant/index'
+  # get 'restaurant/index'
 
-  get 'restaurant/show'
+  # get 'restaurant/show'
 
-  get 'restaurant/new'
+  # get 'restaurant/new'
 
-  get 'restaurant/create'
+  # get 'restaurant/create'
 
- resources :restaurants
+ resources :restaurants do
+    resources :reviews, only: [:new, :create]
+ end
 end
